9a2abe9d93fd2b32aa5ba2a2e545051c62554380
Neto Lourenço <otaviojnetoo@gmail.com>
2025-03-12 21:27:50 -0300

    removendo o q não é usado

--- src/Page/Home/Home.tsx
+++ src/Page/Home/Home.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import * as S from "./styles";
-import Hero from "./Section/Hero";
 import About from "./Section/About";
+import Hero from "./Section/Hero";
 
 const Home: React.FC = () => {
   return (
